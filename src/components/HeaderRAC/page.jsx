1248effe97152373b4ec115e8d6a77deecfd0f49
suchmolin <suchmolin11@gmail.com>
2024-09-30 14:57:53 -0400

    [fix] resp celular2

--- src/components/HeaderRAC/page.jsx
+++ src/components/HeaderRAC/page.jsx
@@ -2,7 +2,7 @@ import Image from "next/legacy/image"
 
 export default function HeaderRAC() {
   return (
-    <div className="w-full h-[660px] sm:h-[720px] flex justify-center mt-10 ">
+    <div className="w-full h-[650px] xs:h-[680px] sm:h-[720px] flex justify-center mt-10 ">
       <div className="w-4/12 lg:w-3/12 h-full hidden sm:flex flex-col items-center justify-center">
         <div className="relative w-[200px] lg:w-[180px] xl:w-[200px] h-[300px] lg:h-[200px] xl:h-[300px] mb-7">
           <Image
